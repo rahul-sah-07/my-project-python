c74400516b23dc67c5cd53f6e42a8185fe900e1d
Rahul Kumar Sah <s7703838@gmail.com>
2025-11-27 11:58:26 +0530

    Update and rename README.md to project-1.py

---
+++ project-1.py
@@ -0,0 +1,18 @@
+# python
+# Day1 -check positive/negative and check even/
+
+num = int(input("Enter a number: "))
+
+if num > 0:
+    print("Number is Positive")
+elif num < 0:
+    print("Number is Negative")
+else:
+    print("Number is Zero")
+
+# Check even or odd for non-zero values
+if num != 0:
+    if num % 2 == 0:
+        print("It is an Even number")
+    else:
+        print("It is an Odd number")
